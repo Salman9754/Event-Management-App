36c04d1fa7d03cda3073128536ed5b59e96e07dc
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-05-25 19:04:46 +0500

    Update DashboardLayout.jsx

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,10 +1,10 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/SideBar";
 import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
     <div className="flex md:h-screen">
-      <Sidebar className='md:sticky md:top-0 md:h-screen'/>
+      <Sidebar className="md:sticky md:top-0 md:h-screen" />
       <main className="flex-1 p-6 min-h-screen bg-neutral-50 overflow-y-auto">
         <Outlet /> {/* This is where the content of the selected nav appears */}
       </main>
