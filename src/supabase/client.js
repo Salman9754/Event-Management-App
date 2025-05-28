e9479aaed5e2bba635dd8e9c02a2e84d90620732
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-05-28 11:35:10 +0500

    env

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const url = import.meta.env.VITE_SUPABASE_URL;
 const SupabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log(SupabaseKey)
 const supabase = createClient(url, SupabaseKey);
 export default supabase;
