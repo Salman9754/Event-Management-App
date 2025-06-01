20bb6fb982a864e3b68cea54650ab6f12147e393
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-06-01 15:36:44 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import { useEffect } from "react";
 import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
-  useEffect(() => {
-    console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
-    console.log("KEY:", import.meta.env.VITE_SUPABASE_KEY);
-  }, []);
-
   return (
     <>
       <AppRoutes />
